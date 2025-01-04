c2d9ef539882d75483023a57cfc870113d4fcfd3
Tom Nyambu <mwakionyambu57@gmail.com>
2025-01-04 15:48:29 +0300

    urls bugfix #1

--- apps/accounts/urls/user_mgt.py
+++ apps/accounts/urls/user_mgt.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 from apps.accounts.views import (
     UpdateUserView,
     TestProtectedView,
@@ -7,12 +6,10 @@ from apps.accounts.views import (
     WelcomeView
 )
 
-router = DefaultRouter()
-router.register('users', AdminUserViewSet, basename='user')
 
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome'),
     path('profile/', UpdateUserView.as_view(), name='update-profile'),
     path('protected/', TestProtectedView.as_view(), name='protected'),
     path('user-details/', AdminUserViewSet.as_view({'get': 'list'}), name='user-details'),
-] + router.urls
+]
